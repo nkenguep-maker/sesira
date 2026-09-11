f360d89dafd82e5f45ba6a6eb428fd58d328bd96
nkenguep-maker <nkengue.p@gmail.com>
2026-09-11 10:32:37 -0700

    ui: load focused form polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ import "./product-redesign.css";
 import "./product-redesign-extras.css";
 import "./product-redesign-patches.css";
 import "./product-polish.css";
+import "./product-polish-forms.css";
 
 const hanken = Hanken_Grotesk({ subsets: ["latin"], variable: "--font-hanken", display: "swap" });
 const jetbrains = JetBrains_Mono({ subsets: ["latin"], variable: "--font-jetbrains", display: "swap" });
